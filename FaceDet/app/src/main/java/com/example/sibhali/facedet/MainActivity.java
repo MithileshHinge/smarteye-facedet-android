b073658d09a3ef35296a0e36187709906e360b49
MithileshHinge <mithhinge@gmail.com>
2017-01-16 00:42:50 +0530

    toggle button added and notification issues fixes
    MainActivity.java - Notification Start/Stop buttons replaced by toggle
    button
    NotifyService.java - removed NullPointerException caused because
    MainActivity didn’t exist when app was closed/minimized

--- FaceDet/app/src/main/java/com/example/sibhali/facedet/MainActivity.java
+++ FaceDet/app/src/main/java/com/example/sibhali/facedet/MainActivity.java
@@ -8,8 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     public static ImageView jIV;
@@ -29,37 +31,28 @@ public class MainActivity extends AppCompatActivity {
         jIV = (ImageView) findViewById(R.id.xIV);
         jIP = (EditText) findViewById(R.id.xIP);
         jIP.setText(lastIP);
-        Button xB = (Button) findViewById(R.id.xB);
-        final Button xBStop = (Button) findViewById(R.id.xBStop);
+        ToggleButton xB = (ToggleButton) findViewById(R.id.xB);
         assert xB != null;
-        assert xBStop != null;
 
 
-        xB.setOnClickListener(new View.OnClickListener() {
+        xB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                servername = jIP.getText().toString();
-                SharedPreferences sp = getSharedPreferences("myPrefs", MODE_PRIVATE);
-                SharedPreferences.Editor editor = sp.edit();
-                editor.putString("Pref_IP", servername);
-                editor.commit();
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    servername = jIP.getText().toString();
+                    SharedPreferences sp = getSharedPreferences("myPrefs", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putString("Pref_IP", servername);
+                    editor.commit();
 
-                Intent intent = new Intent(getBaseContext(), com.example.sibhali.facedet.NotifyService.class);
-                MainActivity.this.startService(intent);
-
-                xBStop.setVisibility(View.VISIBLE);
-
-            }
-        }
-        );
-
-        xBStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction(NotifyService.ACTION);
-                intent.putExtra("RQS", NotifyService.RQS_STOP_SERVICE);
-                sendBroadcast(intent);
+                    Intent intent = new Intent(getBaseContext(), NotifyService.class);
+                    MainActivity.this.startService(intent);
+                }else {
+                    Intent intent = new Intent();
+                    intent.setAction(NotifyService.ACTION);
+                    intent.putExtra("RQS", NotifyService.RQS_STOP_SERVICE);
+                    sendBroadcast(intent);
+                }
             }
         });
     }
